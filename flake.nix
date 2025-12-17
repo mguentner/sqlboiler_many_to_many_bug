77066a878df46f9625387495b17bfcd2852d1051
Maximilian Güntner <code@mguentner.de>
2025-12-17 22:07:20 +0100

    prepare repo

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [];
+      });
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            name = "default";
+            buildInputs = with pkgs; [
+              go
+              gopls
+              bashInteractive
+              sqlboiler
+              postgresql
+            ];
+          };
+        });
+
+    };
+}
